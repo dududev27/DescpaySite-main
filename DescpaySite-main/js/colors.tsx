2e3a374fc0708ea30cbe056c9e77d425dc02baef
Dudu <dudu27042020@gmail.com>
2022-07-05 12:54:11 -0300

    first commit

---
+++ DescpaySite-main/js/colors.tsx
@@ -0,0 +1,21 @@
+
+
+export const Colors = {
+    Background: '#f2f2f2', 
+    main: '#00FF5F',
+    mainBlackGreen: '#135f00',
+    secundary: '#FD6699',
+    text: '#000000',
+    textSecundaty: '#ffff',
+    shadow: '#e9e9e9'
+}
+
+export const ColorSecundary = {
+    Background: '#000000', 
+    main: '#00FF5F',
+    mainBlackGreen: '#135f00',
+    secundary: '#FD6699',
+    text: '#ffff',
+    textSecundaty: '#000000',
+    shadow: '#e9e9e9'
+}
